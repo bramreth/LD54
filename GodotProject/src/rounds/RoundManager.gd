6738e7e27c8a142e38a20aba5a293396d0a432af
Maximilian Taubert <m.taubert2015@gmail.com>
2023-09-30 19:38:17 +0100

    add constants for special rounds back

--- GodotProject/src/rounds/RoundManager.gd
+++ GodotProject/src/rounds/RoundManager.gd
@@ -29,11 +29,11 @@ func round_dispensed() -> void:
 
 
 func _get_special_round() -> Array:
-	if current_round == 0:
+	if current_round == TUTORIAL_ROUND_1:
 		return scripted_rounds[TUTORIAL_1].map(map_scripted_book)
-	elif current_round == 1:
+	elif current_round == TUTORIAL_ROUND_2:
 		return scripted_rounds[TUTORIAL_2].map(map_scripted_book)
-	elif current_round == 4:
+	elif current_round == FIRST_ALL_ONE_GENRE_ROUND:
 		return _generate_single_genre(BookRes.GENRE.BESTSELLERS)
 	return []
 
@@ -64,6 +64,9 @@ func _on_timer_timeout() -> void:
 	current_round += 1
 	one_color_chance = clamp(one_color_chance + 1, 0, 10)
 
+const TUTORIAL_ROUND_1 := 0
+const TUTORIAL_ROUND_2 := 1
+const FIRST_ALL_ONE_GENRE_ROUND := 4
 
 const TUTORIAL_1 := "tutorial_1"
 const TUTORIAL_2 := "tutorial_2"
