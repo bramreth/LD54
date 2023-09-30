90f7646d3f9d6138cc626f7151864c0ac4a89b8a
Maximilian Taubert <m.taubert2015@gmail.com>
2023-09-30 20:24:32 +0100

    Fixed couroutine bricking the start

--- GodotProject/src/TaskAppraisalLever/task_appraisal_lever.gd
+++ GodotProject/src/TaskAppraisalLever/task_appraisal_lever.gd
@@ -3,10 +3,14 @@ class_name TaskAppraisalLever
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var locked: bool = true
+
 func toggle_highlight() -> void:
+	if locked: return
 	animation_player.stop()
 	animation_player.play("fade_out")
 
 
 func pull() -> void:
+	if locked: return
 	RoundManager.evalutae()
--- GodotProject/src/rounds/Announcer.gd
+++ GodotProject/src/rounds/Announcer.gd
@@ -5,19 +5,32 @@ signal announcement_over()
 
 @onready var player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 var current_priority := -1
+var current_queue := []
 
 func set_announcer_position(pos: Vector3) -> void:
 	player.global_position = pos
 
 
-func play(packet: DialogPacket, priority: int = 0) -> void:
-	if priority <= current_priority: return
+func queue(packets: Array, priority: int = 0) -> void:
+	if priority < current_priority: return
+	if player.playing: player.stop()
 	current_priority = priority
+	current_queue.clear()
+	current_queue += packets
+	play(current_queue.pop_front())
+
+
+func play(packet: DialogPacket) -> void:
+#	if priority <= current_priority: return
+#	current_priority = priority
 	player.stream = packet.audio
 	player.play()
 	announcement.emit(packet)
 
 
 func _on_audio_stream_player_3d_finished() -> void:
-	current_priority = -1
-	announcement_over.emit()
+	if not current_queue.is_empty():
+		play(current_queue.pop_front())
+	else:
+		current_priority = -1
+		announcement_over.emit()
