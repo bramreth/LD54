627e5dc0d2c551e8bc0cb047130cd511acf43677
Maximilian Taubert <m.taubert2015@gmail.com>
2023-09-30 13:24:20 +0100

    Books get mesh stripped away when added to shelf

--- GodotProject/src/BookShelf/BookRack.gd
+++ GodotProject/src/BookShelf/BookRack.gd
@@ -9,6 +9,7 @@ signal full(rack: BookRack, score: int)
 @onready var books_root: Node3D = $Books
 @onready var particles: GPUParticles3D = $GPUParticles3D
 @onready var book_offset: float = $CollisionShape3D.shape.size.x / max_books
+
 @onready var animation_player: AnimationPlayer = $Highlight/AnimationPlayer
 
 var genre: BookRes.GENRE
@@ -30,11 +31,14 @@ func is_full() -> bool: return books.size() >= max_books
 func try_add_book(book: Book) -> void:
 	if is_full(): return
 	if not book.book_res: return
-	book.call_deferred("pick_up")
-	book.global_transform = books_root.global_transform
-	book.rotate_y(PI/2)
-	books.append(book)
-	book.global_position += _calculate_book_offset()
+	var mesh = book.seperate_mesh()
+	books.append(book.book_res)
+	books_root.add_child(mesh)
+	book.queue_free()
+	mesh.global_transform = books_root.global_transform
+	mesh.rotate_z(PI/2)
+	
+	mesh.global_position += _calculate_book_offset()
 	
 	if books.size() == max_books: 
 		full.emit(self, _calculate_score())
@@ -47,13 +51,11 @@ func _calculate_book_offset() -> Vector3:
 
 func _calculate_score() -> int:
 	var score = 0
-	for book in books:
-		var book_res: BookRes = book.book_res
+	for book_res in books:
 		score += int(book_res.sort == sort)
 		score += int(book_res.genre == genre)
 	return score
 
 
 func reset() -> void:
-	for book in books: book.queue_free()
 	books.clear()
