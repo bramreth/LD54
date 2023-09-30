c9b52db16263190c503e832c1288511aaf589801
bramreth <bramreth@gmail.com>
2023-09-30 02:07:11 +0100

    RandomBooksAndSuch

--- GodotProject/src/book/Book.gd
+++ GodotProject/src/book/Book.gd
@@ -4,6 +4,27 @@ class_name Book
 @export var book_res: BookRes
 
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var book_mesh := $BookMesh
+@onready var label_1 := $TempLabel
+@onready var label_2 := $TempLabel2
+
+func _ready() -> void:
+	setup()
+
+func setup() -> void:
+	var book_res = BookRes.create_random()
+	book_mesh.material_override.albedo_color = book_res.get_color()
+	match book_res.sort:
+		BookRes.SORT.TOP:
+			label_1.text = "T"
+			label_2.text = "T"
+		BookRes.SORT.MIDDLE:
+			label_1.text = "M"
+			label_2.text = "M"
+		BookRes.SORT.BOTTOM:
+			label_1.text = "B"
+			label_2.text = "B"
+
 
 func pick_up() -> void:
 	freeze = true
--- GodotProject/src/book/BookRes.gd
+++ GodotProject/src/book/BookRes.gd
@@ -16,3 +16,22 @@ enum SORT {
 @export var name := "Name"
 @export var genre: GENRE = GENRE.BESTSELLERS
 @export var sort: SORT = SORT.TOP
+
+const scifi_color: Color = Color(0.34901961684227, 0.43921568989754, 0.44705882668495)
+const classic_color: Color = Color(0.33333298563957, 0.16470600664616, 0.10588199645281)
+const best_color: Color = Color(0.59215700626373, 0.43529400229454, 0.215685993433)
+
+static func create_random() -> BookRes:
+	var book = BookRes.new()
+	book.genre = GENRE.values().pick_random()
+	book.sort = SORT.values().pick_random()
+	return book
+
+func get_color() -> Color:
+	var color_map := {
+		GENRE.BESTSELLERS: best_color,
+		GENRE.SCIFI: scifi_color,
+		GENRE.CLASSICS: classic_color
+	}
+	return color_map[genre]
+
