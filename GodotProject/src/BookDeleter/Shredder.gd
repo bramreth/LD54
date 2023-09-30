69ce3ab19b7002222437e59967b1bd02385f429d
Maximilian Taubert <m.taubert2015@gmail.com>
2023-10-01 00:48:26 +0100

    shredder noises, cursed

--- GodotProject/src/BookDeleter/Shredder.gd
+++ GodotProject/src/BookDeleter/Shredder.gd
@@ -3,10 +3,26 @@ extends Node3D
 @onready var blades: Node3D = $Blades
 @onready var blades_2: Node3D = $Blades2
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var face_animation_player: AnimationPlayer = $CSGBox3D/CSGBox3D2/AnimatedSprite3D/FaceAnimationPlayer
+@onready var voice: AudioStreamPlayer3D = $CSGBox3D/CSGBox3D2/AnimatedSprite3D/Voice
+@onready var face: Sprite3D = $CSGBox3D/CSGBox3D2/AnimatedSprite3D/Face
+@onready var timer: Timer = $CSGBox3D/CSGBox3D2/AnimatedSprite3D/Timer
+
+var quotes = [
+	preload("res://src/Audio/files/more_food.mp3"),
+	preload("res://src/Audio/files/hungry.mp3")
+]
+var nomnom = preload("res://src/Audio/files/nomnom.mp3")
+
+var hungry_face = preload("res://MashaAssets/hungry-face/Untitled_Artwork-2 3.png")
+var normal_face = preload("res://MashaAssets/hungry-face/Untitled_Artwork-3 3.png")
+var happy_face = preload("res://MashaAssets/hungry-face/Untitled_Artwork-4 3.png")
 
 
 func _ready() -> void:
-	pass
+	timer.wait_time = randf_range(4.0, 15.0)
+	timer.start()
+
 
 
 func _process(delta: float) -> void:
@@ -16,3 +32,29 @@ func _process(delta: float) -> void:
 
 func _on_shredding() -> void:
 	animation_player.play("shred", -1, 2.0)
+	face_animation_player.play("shred")
+	face.texture = happy_face
+	callout(nomnom)
+	
+
+
+func callout(stream = quotes.pick_random()) -> void:
+	face.texture = hungry_face
+	voice.stream = stream
+	voice.play()
+
+
+
+func _on_voice_finished() -> void:
+	face.texture = normal_face
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "shred":
+		face.texture = normal_face
+		face_animation_player.play("idle")
+
+
+func _on_timer_timeout() -> void:
+	callout()
+	timer.wait_time = randf_range(4.0, 15.0)
