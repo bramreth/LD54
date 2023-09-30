9cb0765ec36f1fb2c9e400ecc55f0b19f13fa8ec
bramreth <bramreth@gmail.com>
2023-09-30 10:51:07 +0100

    Hand animations

--- GodotProject/src/player/Hands.gd
+++ GodotProject/src/player/Hands.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
 @export var grip_strength := 10.0
+@export var player: CharacterBody3D
 
 @onready var raycast: RayCast3D = $RayCast3D
 @onready var hand_target: Node3D = $HandTarget
-@onready var remote := $HandTarget/RemoteTransform3D
+@onready var remote := $HandTarget/FloatingRemote
+@onready var debug_hand: MeshInstance3D = $HandTarget/DebugHand
+
+
 var current_book: Book = null
 var last_highlighted: Book = null
 
@@ -19,7 +23,7 @@ func _highlight_books() -> void:
 		if last_highlighted:
 			last_highlighted.toggle_highlight(false)
 			last_highlighted = null
-	
+
 	if collider and collider is Book:
 		if last_highlighted != collider and last_highlighted:
 			last_highlighted.toggle_highlight(false)
@@ -29,7 +33,6 @@ func _highlight_books() -> void:
 			last_highlighted = collider
 			last_highlighted.toggle_highlight(true)
 
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
 		var collider: Object = raycast.get_collider()
@@ -37,10 +40,16 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("throw"):
 		if current_book: _drop_book(current_book)
 
+var hand_tween: Tween
 
 func _pickup_book(book: Book) -> void:
+	var book_position = book.global_position
 	book.pick_up()
 	current_book = book
+	if is_instance_valid(hand_tween) and hand_tween.is_running():
+		hand_tween.kill()
+	hand_tween = create_tween().set_trans(Tween.TRANS_SPRING).set_ease(Tween.EASE_OUT)
+	hand_tween.tween_property(debug_hand, "global_position", hand_target.global_position, 0.2).from(book.global_position)
 	remote.remote_path = remote.get_path_to(book)
 
 
