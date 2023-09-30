12af638e1cb7a67d4154c70395b3211e58e3d2b5
Maximilian Taubert <m.taubert2015@gmail.com>
2023-09-30 01:31:12 +0100

    More book stuff

--- GodotProject/src/book/Book.gd
+++ GodotProject/src/book/Book.gd
@@ -1,6 +1,16 @@
 extends RigidBody3D
+class_name Book
 
 @export var book_res: BookRes
 
-func _ready() -> void:
-	pass
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+
+func pick_up() -> void:
+	freeze = true
+	collision_shape.disabled = true
+
+
+func drop(impulse: Vector3 = Vector3.ZERO) -> void:
+	freeze = false
+	collision_shape.disabled = false
+	apply_central_impulse(impulse)
---
+++ GodotProject/src/player/Hands.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@export var grip_strength := 10.0
+
+@onready var raycast: RayCast3D = $RayCast3D
+@onready var hand_target: Node3D = $HandTarget
+
+var current_book: Book = null
+
+func _process(delta: float) -> void:
+	if current_book:
+		current_book.global_transform = current_book.global_transform.interpolate_with(hand_target.global_transform, delta * grip_strength)
+		current_book.look_at(self.global_position)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		var collider: Object = raycast.get_collider()
+		if collider and collider is Book: _pickup_book(collider)
+	if event.is_action_pressed("throw"):
+		if current_book: _drop_book(current_book)
+
+
+func _pickup_book(book: Book) -> void:
+	book.pick_up()
+	current_book = book
+
+
+func _drop_book(book: Book) -> void: 
+	book.drop(global_position.direction_to(book.global_position).normalized())
+	current_book = null
