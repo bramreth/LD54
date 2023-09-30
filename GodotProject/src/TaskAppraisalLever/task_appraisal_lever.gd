32c669ad8fc933b2e80679a8d4fdf6b673a6c1cd
bramreth <bramreth@gmail.com>
2023-09-30 21:16:07 +0100

    lever improvements

--- GodotProject/src/TaskAppraisalLever/task_appraisal_lever.gd
+++ GodotProject/src/TaskAppraisalLever/task_appraisal_lever.gd
@@ -2,6 +2,7 @@ extends StaticBody3D
 class_name TaskAppraisalLever
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var lever_animator: AnimationPlayer = $LeverAnimator
 
 func toggle_highlight() -> void:
 	animation_player.stop()
@@ -9,4 +10,5 @@ func toggle_highlight() -> void:
 
 
 func pull() -> void:
+	lever_animator.play("Pull")
 	RoundManager.evalutae()
