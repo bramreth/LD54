519272cc25801c539dc648366b0995607d2cadfe
bramreth <bramreth@gmail.com>
2023-10-01 14:07:35 +0100

    book back stuff

--- GodotProject/src/book/Book.gd
+++ GodotProject/src/book/Book.gd
@@ -5,39 +5,56 @@ class_name Book
 
 @export var classic_books: Array[CompressedTexture2D] = [
 	preload("res://MashaAssets/Dracula.png"),
-	preload("res://MashaAssets/Great Expectations.png"),
-	preload("res://MashaAssets/Jane Eyre.png"),
 	preload("res://MashaAssets/North and South.png"),
+	preload("res://MashaAssets/The Picture of Dorian Gray.png")
+]
+
+@export var classic_books_top: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Great Expectations.png"),
 	preload("res://MashaAssets/Tess of d'Urbervilles.png"),
+	preload("res://MashaAssets/Wide Sargasso Sea.png")
+]
+
+@export var classic_books_bottom: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Jane Eyre.png"),
 	preload("res://MashaAssets/The Adventures of Sherlock Holmes.png"),
-	preload("res://MashaAssets/The Picture of Dorian Gray.png"),
-	preload("res://MashaAssets/Wide Sargasso Sea.png"),
 	preload("res://MashaAssets/Wuthering Heights.png")
 ]
 
 @export var scifi_books: Array[CompressedTexture2D] = [
-	preload("res://MashaAssets/Annihilation.png"),
 	preload("res://MashaAssets/Children of Time.png"),
-	preload("res://MashaAssets/Dune.png"),
-	preload("res://MashaAssets/Frankenstein.png"),
-	preload("res://MashaAssets/Ringworld.png"),
-	preload("res://MashaAssets/The Hitcher's Guide to the Galaxy.png"),
 	preload("res://MashaAssets/The Martian.png"),
-	preload("res://MashaAssets/The Stand.png"),
 	preload("res://MashaAssets/The War of the Worlds.png"),
 	preload("res://MashaAssets/Gideon the Ninth.png")
+]
 
+@export var scifi_books_top: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Annihilation.png"),
+	preload("res://MashaAssets/Ringworld.png"),
+	preload("res://MashaAssets/The Stand.png"),
+]
+
+@export var scifi_books_bottom: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Dune.png"),
+	preload("res://MashaAssets/Frankenstein.png"),
+	preload("res://MashaAssets/The Hitcher's Guide to the Galaxy.png")
 ]
 
 @export var best_books: Array[CompressedTexture2D] = [
-	preload("res://MashaAssets/Borris's Web.png"),
 	preload("res://MashaAssets/Giant Women.png"),
-	preload("res://MashaAssets/Greener Grass.png"),
-	preload("res://MashaAssets/Lord of the Mosquitoes.png"),
-	preload("res://MashaAssets/Murderous Mockingbird.png"),
-	preload("res://MashaAssets/Sand Pile.png"),
+	preload("res://MashaAssets/Murderous Mockingbird.png")
+]
+
+@export var best_books_top: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Borris's Web.png"),
 	preload("res://MashaAssets/The Sunrise Library.png"),
 	preload("res://MashaAssets/Windy Wendy Leaves Her Pan.png")
+]
+
+@export var best_books_bottom: Array[CompressedTexture2D] = [
+	preload("res://MashaAssets/Greener Grass.png"),
+	preload("res://MashaAssets/Lord of the Mosquitoes.png"),
+	preload("res://MashaAssets/Sand Pile.png"),
 ]
 
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
@@ -54,11 +71,30 @@ func setup(book_res_in: BookRes) -> void:
 	var book_texture: CompressedTexture2D
 	match book_res.genre:
 		BookRes.GENRE.BESTSELLERS:
+			match book_res.sort:
+				BookRes.SORT.TOP:
+					pass
+				BookRes.SORT.MIDDLE:
+					pass
+				BookRes.SORT.BOTTOM:
+					pass
 			book_texture = best_books.pick_random()
 		BookRes.GENRE.SCIFI:
-			book_texture = scifi_books.pick_random()
+			match book_res.sort:
+				BookRes.SORT.TOP:
+					book_texture = scifi_books_top.pick_random()
+				BookRes.SORT.MIDDLE:
+					book_texture = scifi_books.pick_random()
+				BookRes.SORT.BOTTOM:
+					book_texture = scifi_books_bottom.pick_random()
 		BookRes.GENRE.CLASSICS:
-			book_texture = classic_books.pick_random()
+			match book_res.sort:
+				BookRes.SORT.TOP:
+					book_texture = classic_books_top.pick_random()
+				BookRes.SORT.MIDDLE:
+					book_texture = classic_books.pick_random()
+				BookRes.SORT.BOTTOM:
+					book_texture = classic_books_bottom.pick_random()
 	book_mesh.get_surface_override_material(0).set_shader_parameter("book_texture", book_texture)
 	var book_name = book_texture.load_path
 	book_name = book_name.rsplit(".png")[0]
