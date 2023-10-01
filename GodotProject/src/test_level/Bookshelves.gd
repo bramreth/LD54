b23b1be105aaf6c36d6639f240e75f8be74a56be
Maximilian Taubert <m.taubert2015@gmail.com>
2023-10-01 12:36:27 +0100

    Fixed shelf visibility scripting

--- GodotProject/src/test_level/Bookshelves.gd
+++ GodotProject/src/test_level/Bookshelves.gd
@@ -9,18 +9,23 @@ func _ready() -> void:
 func round_started(round: int, _books: Array) -> void:
 	match(round):
 		RoundManager.TUTORIAL_ROUND_1: show_tutorial_shelf()
-		RoundManager.TUTORIAL_ROUND_2: show_first_area()
+		RoundManager.TUTORIAL_ROUND_2: pass
+		2: show_totorial_row()
+		3: show_first_area()
 		_: pass
 
 
 func show_tutorial_shelf() -> void:
-	initial_bookrow.get_shelf_for_genre(BookRes.GENRE.CLASSICS).reset()
+	initial_bookrow.get_shelf_for_genre(BookRes.GENRE.CLASSICS).front().reset()
+
+
+func show_totorial_row() -> void:
+	var row = get_tree().get_first_node_in_group("zone_1_tutorial_bookrow")
+	row.get_shelf_for_genre(BookRes.GENRE.CLASSICS).back().reset()
+	row.get_shelf_for_genre(BookRes.GENRE.BESTSELLERS).front().reset()
+
 
 
 func show_first_area() -> void:
 	for row in get_tree().get_nodes_in_group("zone_1_bookrow"):
-		if initial_bookrow:
-			row.get_shelf_for_genre(BookRes.GENRE.SCIFI).reset()
-			row.get_shelf_for_genre(BookRes.GENRE.BESTSELLERS).reset()
-		else:
-			row.reset_all()
+		row.reset_all()
