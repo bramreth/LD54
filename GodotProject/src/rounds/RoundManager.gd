cf036f60de39fdff005ae48ae1c13edae82074df
Maximilian Taubert <m.taubert2015@gmail.com>
2023-10-02 19:44:05 +0100

    round 8 crash

--- GodotProject/src/rounds/RoundManager.gd
+++ GodotProject/src/rounds/RoundManager.gd
@@ -13,7 +13,8 @@ var total_score: Dictionary = {}
 
 
 func start_game() -> void:
-	current_round = 0
+	current_round = 9
+	UiEventBus.lock_lever.emit(false)
 	total_score.clear()
 	timer.start(1.0)
 
@@ -44,14 +45,14 @@ func calculate_total_score() -> float:
 	var rounds = total_score.keys().size()
 	var total_score_value: float = 0.0
 	for key in total_score.keys():
-		total_score += total_score[key][SCORE]
-	return total_score_value / rounds
+		total_score_value += total_score[key][SCORE]
+	return total_score_value * 100 / rounds
 
 
 func calculate_total_time() -> float:
 	var total_time: float = 0.0
 	for key in total_score.keys():
-		total_score += total_score[key][TIME]
+		total_time += total_score[key][TIME]
 	return total_time
 
 
@@ -83,9 +84,9 @@ func _generate_single_genre(
 	sort
 ) -> Array:
 	var books := []
+	if genre == null: genre = BookRes.GENRE.values().pick_random()
+	if sort == null: sort = BookRes.SORT.values().pick_random()
 	for i in range(amount):
-		if genre == null: BookRes.GENRE.values().pick_random()
-		if sort == null: BookRes.SORT.values().pick_random()
 		books.append(BookRes.create_from_genre(genre, sort))
 	return books
 
