b4aa762e1f69c3d0dd81490d016e96fe39851de8
Maximilian Taubert <m.taubert2015@gmail.com>
2023-10-02 20:15:28 +0100

    shredder disables after 5 bnooks per round

--- GodotProject/src/BookDeleter/ShredderArea.gd
+++ GodotProject/src/BookDeleter/ShredderArea.gd
@@ -6,17 +6,24 @@ signal shredding()
 @onready var highlight: MeshInstance3D = $"../CSGBox3D/Highlight"
 @onready var animation_player: AnimationPlayer = $"../CSGBox3D/AnimationPlayer"
 
+var disabled := false
+
+func disable() -> void: disabled = true
+func enable() -> void: disabled = false
+
 
 func _ready() -> void:
 	highlight.transparency = 1.0
 
 
 func toggle_highlight() -> void:
+	if disabled: return
 	animation_player.stop()
 	animation_player.play("fade_out")
 
 
 func use(book: Book) -> void:
+	if disabled: return
 	if book:
 		book.destroy()
 		shredding.emit()
@@ -24,4 +31,5 @@ func use(book: Book) -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Book:
-		use(body)
+		if not disabled: use(body)
+		else: body.apply_central_impulse(Vector3.UP * 2)
